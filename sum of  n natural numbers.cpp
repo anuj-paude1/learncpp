01eae1f83ebc199f37aaa338e023534595a1adb4
Anuj Paudel <anujpaudel98@yahoo.com>
2023-01-19 19:32:20 +0545

    initial

---
+++ sum of  n natural numbers.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    cout<<"Sum of first "<<n<<" natural number is "<< (n*(n+1))/2 << endl;
+
+    return 0;
+}
