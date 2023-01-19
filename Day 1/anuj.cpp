6b81e374332e85df847e9e642f1620b8ddd9fe5a
anuj-paude1 <123000988+anuj-paude1@users.noreply.github.com>
2023-01-19 19:24:43 +0545

    Update anuj.cpp

--- Day 1/anuj.cpp
+++ Day 1/anuj.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
-int main(void)
+int
+main ()
 {
-    int i;
-    cout<<"Enter a Number";
+  int a, b, c;
+  std::cout << "Enter any three numbers";
+  std::cin >> a >> b >> c ;
+  if (a > b && a > c)
+    {
+      std::cout << a << "is the largest" << std::endl;
+    }
+  else if (b > a && b > c)
+    {
+      std::cout << b << "is the largest" << std::endl;
+    }
+  else
+    {
+      std::cout << c << " the largest" << std::endl;
+    }
+
 }
+
